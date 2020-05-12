9c52de49433573d2a8bb940a536cc277b4280043
brendena <cerebralasylum1@gmail.com>
2020-05-12 18:51:29 -0400

    got yargs into the project

---
+++ config-overrides.js
@@ -0,0 +1,22 @@
+
+const webpack = require('webpack');
+const path = require('path');
+
+
+module.exports = function override(config, env) {
+    console.log(require.resolve('browserfs'))
+    config.resolve.alias['fs'] = 'browserfs/dist/shims/fs.js';
+    config.resolve.alias['buffer'] = 'browserfs/dist/shims/buffer.js';
+    config.resolve.alias['path'] = 'browserfs/dist/shims/path.js';
+    config.resolve.alias['processGlobal'] = 'browserfs/dist/shims/process.js';
+    config.resolve.alias['bufferGlobal'] = 'browserfs/dist/shims/bufferGlobal.js';
+    config.resolve.alias['bfsGlobal'] = require.resolve('browserfs');
+
+
+    config.plugins.push(new webpack.ProvidePlugin({ BrowserFS: 'bfsGlobal', process: 'processGlobal', Buffer: 'bufferGlobal' }))
+    config.node.process = false;
+    config.node.Buffer = false;
+    //config.module.noParse =  /browserfs\.js/
+    
+    return config;
+}
--- src/components/XtermJS.tsx
+++ src/components/XtermJS.tsx
@@ -3,17 +3,27 @@ import { Terminal } from 'xterm';
 import XtermManager from '../objects/XtermManager'
 import yargsBrowser from 'yargs_browser/dist/index'
 
+
 import '../../node_modules/xterm/css/xterm.css';
 import './XtermJS.css';
 
-class fs 
-{
-    readFileSync(directory:string,local:string){
-        console.log("tested")
-    };
-}
-var yargs =  require('yargs')
+var BrowserFS = require("browserfs")
+var fs = BrowserFS.BFSRequire('fs');
+BrowserFS.configure({ fs: "LocalStorage" }, function (e) {
+  console.log(e);
+  console.log("e is nothing")
+});
 
+var yargs =  require('yargs')
+/*
+var yargs =  require('yargs')
+var BrowserFS = require("browserfs")
+var fs = BrowserFS.BFSRequire('fs');
+BrowserFS.configure({ fs: "LocalStorage" }, function (e) {
+  console.log(e);
+  console.log("e is nothing")
+});
+*/
 
 //https://medium.com/codingtown/xterm-js-terminal-2b19ccd2a52
 //light wrapper for xterm
@@ -35,6 +45,7 @@ const XtermJS: React.FC = () => {
             setTimeout(function(){ 
                 var xtermManger = new XtermManager(container.current,(newLine:string)=>{
                     console.log(newLine);
+                    //*
                     var argv = yargs.locale("en")
                     
                     .command("test","asdfasdf",()=>{
@@ -43,6 +54,7 @@ const XtermJS: React.FC = () => {
                     parse(['help'],(_err, argv, output)=>{
                         console.log(output);
                     });
+                    //*/
                 });
             }, 
             500);
